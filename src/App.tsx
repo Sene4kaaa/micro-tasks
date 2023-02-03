b612df31336f90860d04a8b3af33f0e2d57e9cdb
Sene4kaaa <senkevich92@mail.ru>
2023-02-03 20:33:42 +0300

    training button(callBack)

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,28 @@
 import React from 'react';
 import './App.css'
-
+import {Button} from "./components/Button";
 
 
 function App() {
-    // const myFirstSubscriber = (event:MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hello Im Vasia!')
-    // }
-    //
-    // const mySecondSubscriber = (event:MouseEvent<HTMLButtonElement> => {
-    //     console.log('Hello Im Ivan!')
-    // }
-
-  /*  const onClickHandler= (name:string)=>{
-       console.log(name)
-    }*/
-    const foo1 = () => {
-// ничего не передавать,но в консоли должно показаться 100200
-        console.log(100200)
+    const Button1Func = (subscriber:string,age:number,adress:string) => {
+        console.log(subscriber,age,adress)
     }
 
-    const foo2 = (num:number) => {
-//передать цифру 100200
-console.log(num)
+    const Button2Func = (subscriber:string) => {
+        console.log(subscriber)
+    }
+
+    const Button3Func = () => {
+        console.log('Im stupid Button')
     }
 
     return (
         <div className="App">
-      {/* <button onClick={(event)=>{.log('Hello')}}>MyYouTubeChanel-1</button>*/}
-      {/* <button onClick={myFirstSubscriber}>MyYouTubeChanel-1</button>*/}
-      {/* <button onClick={(event) => onClickHandler('Vasia')}>MyYouTubeChanel-1</button>
-       <button onClick={(event)=>onClickHandler('Ivan')}>MyYouTubeChanel-2</button>
-       <button onClick={()=>onClickHandler('some info')}>MyYouTubeChanel-3</button>*/}
-
-            <button onClick={foo1}>1</button>
-            <button onClick={()=>foo2(100200)}>2</button>
-  </div>
-
+            <Button name={'MyYouTubeChanel-1'} callBack={()=>Button1Func('Im Vasia',21,'Live in Minsk')}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={()=>Button2Func('Im Ivan')}/>
+            <Button name={'MyYouTubeChanel-3'} callBack={Button3Func}/>
+        </div>
     );
-
 }
 
 export default App;
